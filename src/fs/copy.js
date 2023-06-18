de744a856fa40bdc5683424f4dabd773cd4fee52
Aliaksei Karavaichyk <a.karavaychik@gmail.com>
2023-06-19 01:43:52 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
-const copy = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+export const copy = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const subPath = path.join(__dirname, 'files');
+    const destinationPath = path.join(__dirname, 'files_copy')
+
+    if (!fs.existsSync(subPath) || fs.existsSync(destinationPath)) throw new Error('FS operation failed');
+    else {
+        fs.mkdir(destinationPath ,(err)=>{
+            if (err) {
+                throw new Error('FS operation failed');
+            }
+
+            fs.readdir(subPath, (err, files)=>{
+                if (err) {
+                    throw new Error('FS operation failed');
+                }
+
+                files.forEach(file => fs.copyFile(path.join(subPath, file), path.join(destinationPath, file), (err)=>{
+                    if (err) {
+                        throw new Error('FS operation failed');
+                    }
+                }))
+            })
+        })
+    }
 };
 
-await copy();
+copy();
