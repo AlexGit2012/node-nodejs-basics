f5d13c4794423c9c4c004cfce9b721fb79cf08ea
Aliaksei Karavaichyk <a.karavaychik@gmail.com>
2023-06-19 01:44:43 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
-const list = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+export const list = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const subPath = path.join(__dirname, 'files');
+    fs.readdir(subPath, (err, files)=>{
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+
+        files.forEach(file => console.log(file))
+    })
 };
 
-await list();
+list();
