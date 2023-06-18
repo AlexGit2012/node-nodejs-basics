93b7c17eff3dfb8a36130ce1e3f3b9629c9d998e
Aliaksei Karavaichyk <a.karavaychik@gmail.com>
2023-06-19 01:52:55 +0300

    feat: add functions for work with streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
-const transform = async () => {
-    // Write your code here 
+import {Transform} from 'stream';
+
+export const transform = async () => {
+
+    const reverseString = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''))
+        }
+    })
+
+    process.stdin.pipe(reverseString).pipe(process.stdout);
 };
 
-await transform();
+transform();
