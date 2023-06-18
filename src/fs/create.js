f36f286f80b9e0aebf25124ba999f8923bd51ef4
Aliaksei Karavaichyk <a.karavaychik@gmail.com>
2023-06-19 01:42:36 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
-const create = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+export const create = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const subPath = path.join(__dirname, 'files');
+
+    if (fs.existsSync(path.join(subPath, 'fresh.txt'))) throw new Error('FS operation failed');
+    else {
+        fs.writeFile(path.join(subPath, 'fresh.txt'), "I am fresh and young", (err)=>{
+            if (err) {
+                throw new Error('FS operation failed');
+            }
+        })
+    }
 };
 
-await create();
+create();
