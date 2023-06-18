c4b9ae385e7972aa80b5ec21c386567baf37cb7b
Aliaksei Karavaichyk <a.karavaychik@gmail.com>
2023-06-19 02:00:55 +0300

    feat: add functions for work with archive

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
-const compress = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import zlib from'zlib';
+
+export const compress = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const subPath = path.join(__dirname, 'files');
+    const gzip = zlib.createGzip();
+
+    const readableStream = fs.createReadStream(path.join(subPath, 'fileToCompress.txt'), {encoding: 'utf-8'});
+    const writableStream = fs.createWriteStream(path.join(subPath, 'archive.gz'));
+
+    readableStream.pipe(gzip).pipe(writableStream);
 };
 
-await compress();
+compress();
