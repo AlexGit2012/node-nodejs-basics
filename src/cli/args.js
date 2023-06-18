658c39adebb17466a7618846ba9b6c627bee10fd
Aliaksei Karavaichyk <a.karavaychik@gmail.com>
2023-06-19 01:48:48 +0300

    feat: implement functions work with cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
-const parseArgs = () => {
-    // Write your code here 
+export const parseArgs = () => {
+    let parsedArgv = process.argv.slice(2);
+    let parsedString = '';
+    while (parsedArgv.length) {
+        parsedString+=`${parsedArgv.shift().slice(2)} is ${parsedArgv.shift()}, `
+    }
+    if (parsedString) {
+        parsedString = parsedString.slice(0, parsedString.length - 2);
+    }
+    console.log(parsedString);
+    return parsedString;
 };
 
 parseArgs();
